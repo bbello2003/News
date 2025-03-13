eb5ac9fbf0bc2647790f28adc403f2a48767798c
bbello2003 <ppasikapong@gmail.com>
2025-03-13 22:11:37 +0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Pasika Pongsawaluk on 13/3/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
